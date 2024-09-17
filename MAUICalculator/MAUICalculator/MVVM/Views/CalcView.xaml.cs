88c28e1332cb8ea91e2b097f77396e8bdd107dd5
Davy <davixsmit@gmail.com>
2024-09-17 09:51:13 +0200

    Adds Calculator App

---
+++ MAUICalculator/MAUICalculator/MVVM/Views/CalcView.xaml.cs
@@ -0,0 +1,12 @@
+using MAUICalculator.MVVM.ViewModels;
+
+namespace MAUICalculator.MVVM.Views;
+
+public partial class CalcView : ContentPage
+{
+	public CalcView()
+	{
+		InitializeComponent();
+		BindingContext = new CalcViewModel();
+	}
+}
